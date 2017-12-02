d4528a29b96567e2aedf414e334a0d56d6dfe49a
Nathan Armstrong <kenogu.labz@gmail.com>
2017-12-01 17:05:54 -0800

    Add tests and output to Day 1 solution.

--- day-1.clj
+++ day-1.clj
@@ -15,8 +15,21 @@
     (partition 2 1)
     sum-matching-pairs))
 
+(assert (= (checksum "1122") 3))
+(assert (= (checksum "1111") 4))
+(assert (= (checksum "1234") 0))
+(assert (= (checksum "91212129") 9))
+
 (defn checksum-2
   [x]
   (->> x
     (map #(list %1 %2) (drop (/ (count x) 2) (cycle x)))
     sum-matching-pairs))
+
+(assert (= (checksum-2 "1212") 6))
+(assert (= (checksum-2 "1221") 0))
+(assert (= (checksum-2 "123425") 4))
+(assert (= (checksum-2 "123123") 12))
+(assert (= (checksum-2 "12131415") 4))
+
+(println "Part A: " (checksum input) "\nPart B: " (checksum-2 input))
