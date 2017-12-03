a0a8d38d2933b974e90e4f8e05a1eb1d2b74c6a3
Nathan Armstrong <kenogu.labz@gmail.com>
2017-12-02 21:41:20 -0800

    Raw code from day 3's solution
    Note that part A is basically non-existent - it was done primarily by hand.

---
+++ day-3.clj
@@ -0,0 +1,77 @@
+(def input 361527)
+
+(defn parse-int
+  [n]
+  (try
+    (Integer/parseInt n)
+    (catch Exception e (println "Failed to parse int: " n))))
+
+(defn transpose
+  "Transposes the given nested sequence into nested vectors, as
+  in matrix transposition.  E.g., (transpose [[1 2 3] [4 5 6]])
+  would return [[1 4] [2 5] [3 6]]."
+  [s]
+  (vec (apply map vector s)))
+
+(defn transpose
+  "Transposes the given nested sequence into nested vectors, as
+  in matrix transposition.  E.g., (transpose [[1 2 3] [4 5 6]])
+  would return [[1 4] [2 5] [3 6]]."
+  [s]
+  (vec (apply map vector s)))
+ 
+(defn grid-val
+	[grid [x y]]
+	(get-in grid [x y]))
+
+(defn numeric-grid-val
+	[grid pos]
+	(let [cell-val (grid-val grid pos)]
+	  (if cell-val cell-val 0)))
+
+(defn north [[x y]] [x (inc y)])
+(defn south [[x y]] [x (dec y)])
+(defn east [[x y]] [(inc x) y])
+(defn west [[x y]] [(dec x) y])
+(defn ccw [dir]
+	(case dir
+	  :north :west
+	  :west :south
+	  :south :east
+	  :east :north))
+	 
+(defn move [pos dir]
+	(case dir
+	  :north (north pos)
+	  :south (south pos)
+	  :east (east pos)
+	  :west (west pos)))
+
+(defn try-turn
+	[grid pos dir]
+	(let [test-dir (ccw dir)
+	      test-pos (move pos test-dir)
+	      test-cell (grid-val grid test-pos)]
+	  (if-not test-cell test-dir dir)))
+	 
+(defn surrounding-cell-sum
+	[grid [x y]]
+	(reduce +
+		(for [dx [-1 0 1] dy [-1 0 1]]
+		  (numeric-grid-val grid [(+ x dx) (+ y dy)]))))
+
+(def starting-state [{0 {0 1}} [0 0] :east])
+
+(defn step [grid pos dir]
+	(let [new-pos (move pos dir)
+	      new-dir (try-turn grid new-pos dir)
+	      new-val (surrounding-cell-sum grid new-pos)
+	      new-grid (assoc-in grid new-pos new-val)]
+	  [new-grid new-pos new-dir]))
+
+(defn step-until [grid pos dir max-val]
+	(let [curr-val (grid-val grid pos)]
+	  (if (> curr-val max-val)
+	      curr-val
+	      (let [[new-grid new-pos new-dir] (step grid pos dir)]
+	         (recur new-grid new-pos new-dir max-val)))))
