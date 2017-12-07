3d3ae99291df50d657b57b4b059b61b77175eea3
Nathan Armstrong <kenogu.labz@gmail.com>
2017-12-07 02:53:36 -0800

    Tidy up formatting; give actual solution func

--- day-7.clj
+++ day-7.clj
@@ -1,9 +1,9 @@
 (ns advent-of-code-2017.day-7
-    (:require [clojure.string :as str]
-              [clojure.set :as set]))
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
 
 (def input
-	"nzyiue (57)
+  "nzyiue (57)
 pdmkag (39)
 bogbg (13)
 nubay (45)
@@ -1620,77 +1620,74 @@ tqlentr (214) -> gfxnuuk, thmlk")
 (def standalone-program-re #"^([a-z]+) \((\d+)\)$")
 
 (defn parse-supporting-program-line
-	[line]
-	(if-let [matches (re-matches supporting-program-re line)]
-	  (let [[_ tower weight subtowers-str] matches
-	        subtowers (str/split subtowers-str #", ")]
-	    [tower {:weight (parse-int weight) :subtowers subtowers}])))
+  [line]
+  (if-let [matches (re-matches supporting-program-re line)]
+    (let [[_ tower weight subtowers-str] matches
+          subtowers (str/split subtowers-str #", ")]
+      [tower {:weight (parse-int weight) :subtowers subtowers}])))
 
 (defn parse-standalone-program-line
-	[line]
-	(let [matches (re-matches standalone-program-re line)
-	      [_ tower weight] matches]
-	  [tower {:weight (parse-int weight) :subtowers []}]))
+  [line]
+  (let [matches (re-matches standalone-program-re line)
+        [_ tower weight] matches]
+    [tower {:weight (parse-int weight) :subtowers []}]))
 
 (defn parse-line
-	[line]
-	(if-let [supporting-program (parse-supporting-program-line line)]
-	  supporting-program
-	  (parse-standalone-program-line line)))
+  [line]
+  (if-let [supporting-program (parse-supporting-program-line line)]
+    supporting-program
+    (parse-standalone-program-line line)))
 
-(defn all-towers
-	[tower-defs]
-	(->> tower-defs
-	  (map first)
-	  (into #{})))
+(defn all-programs
+  [program-defs]
+  (->> program-defs
+    (map first)
+    (into #{})))
 
 (defn all-subtowers
-	[tower-defs]
-	(->> tower-defs
-	  (map #(get-in % [1 :subtowers]))
-	  (reduce into #{})))
+  [program-defs]
+  (->> program-defs
+    (map #(get-in % [1 :subtowers]))
+    (reduce into #{})))
 
-(defn base-tower
-	[tower-defs]
-	(first (set/difference (all-towers tower-defs)
-	                       (all-subtowers tower-defs))))
+(defn base-program
+  [program-defs]
+  (first (set/difference (all-programs program-defs)
+                         (all-subtowers program-defs))))
 
-(defn bottom-up-tree
-	[tower-defs]
-	(into {} tower-defs))
+(defn program-tree
+  [program-defs]
+  (into {} program-defs))
 
 (defn subtowers-of
-	[tree program]
-	(get-in tree [program :subtowers]))
+  [tree program]
+  (get-in tree [program :subtowers]))
 
 (defn weight-of
-	[tree program]
-	(get-in tree [program :weight]))
+  [tree program]
+  (get-in tree [program :weight]))
  
 (defn aggregate-weight
-  ([tower-defs]
-   (aggregate-weight (bottom-up-tree tower-defs) (base-tower tower-defs)))
-	
-  ([tree tower]
-   (let [program-weight (weight-of tree tower)
-         subtowers (subtowers-of tree tower)
-         aggregated-subtrees (reduce aggregate-weight tree subtowers)
-         new-subtree-weights (map #(weight-of aggregated-subtrees %) subtowers)
-	       new-weight (reduce + program-weight new-subtree-weights)]
-     (assoc-in aggregated-subtrees [tower :weight] new-weight))))
+  [tree tower]
+  (let [program-weight (weight-of tree tower)
+        subtowers (subtowers-of tree tower)
+        aggregated-subtrees (reduce aggregate-weight tree subtowers)
+        new-subtree-weights (map #(weight-of aggregated-subtrees %) subtowers)
+        new-weight (reduce + program-weight new-subtree-weights)]
+    (assoc-in aggregated-subtrees [tower :weight] new-weight)))
 
 (defn reverse-lookup
-	[m v]
-	(->> m
+  [m v]
+  (->> m
     (filter #(= v (second %)))
     first
     first))
 
 (defn unbalanced
-	[subtowers subtower-weights]
-	(let [freqs (frequencies subtower-weights)
-	      diff-weight (reverse-lookup freqs 1)
-	      subtowers-to-weights (map vector subtowers subtower-weights)]
+  [subtowers subtower-weights]
+  (let [freqs (frequencies subtower-weights)
+        diff-weight (reverse-lookup freqs 1)
+        subtowers-to-weights (map vector subtowers subtower-weights)]
     (reverse-lookup subtowers-to-weights diff-weight)))
 
 
@@ -1710,11 +1707,20 @@ tqlentr (214) -> gfxnuuk, thmlk")
        (let [[program-a program-b] subtowers
              [weight-a weight-b] (map #(weight-of tree %) subtowers)]
          (cond
-         	 (= weight-a weight-b) [program diff]
-         	 (= (+ weight-a diff) weight-b) (seek-misweighted tree program-a diff)
-         	 :else (seek-misweighted tree program-b diff)))
+           (= weight-a weight-b) [program diff]
+           (= (+ weight-a diff) weight-b) (seek-misweighted tree program-a diff)
+           :else (seek-misweighted tree program-b diff)))
        (let [subtower-weights (map #(weight-of tree %) subtowers)
              unbalanced-program (unbalanced subtowers subtower-weights)]
          (if-not unbalanced-program
            [program diff]
            (seek-misweighted tree unbalanced-program diff)))))))
+
+(defn rebalanced-weight
+  [input]
+  (let [parsed-defs (map parse-line (str/split-lines input))
+        base (base-program parsed-defs)
+        regular-tree (program-tree parsed-defs)
+        aggregate-tree (aggregate-weight regular-tree base)
+        [misweighted-program diff-weight] (seek-misweighted aggregate-tree base)]
+    (+ (weight-of regular-tree misweighted-program) diff-weight)))
