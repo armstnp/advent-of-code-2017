d63811e7dcd097c157d0fc5dcb1e595487fbb0c7
Nathan Armstrong <kenogu.labz@gmail.com>
2017-12-09 22:56:34 -0800

    Refactor day 10.

--- day-10.clj
+++ day-10.clj
@@ -1,46 +1,78 @@
 (def input [106,118,236,1,130,0,235,254,59,205,2,87,129,25,255,118])
 (def str-input "106,118,236,1,130,0,235,254,59,205,2,87,129,25,255,118")
- 
-(defn step
-  [{:keys [hash-list current-pos skip-size lengths] :as state}]
-  (let [list-length (count hash-list)
-        circular-list (cycle hash-list)
-        [curr-length & rest-lengths] lengths
-        reversed-section (reverse (take curr-length (drop current-pos circular-list)))
-        initial-replacement (concat (take current-pos hash-list)
-                                    reversed-section
-                                    (drop (+ current-pos curr-length) hash-list))
-        [base-replacement extra-at-end] (split-at list-length initial-replacement)
-        cycled-replacement (concat extra-at-end
-                                   (drop (count extra-at-end) base-replacement))]
-    {:hash-list cycled-replacement
-     :current-pos (mod (+ current-pos curr-length skip-size) list-length)
-     :skip-size (inc skip-size)
+
+(def ring-length 256)
+(def starting-ring (range ring-length))
+
+(def hash-lengths-suffix [17 31 73 47 23])
+
+(defn twist
+  [{:keys [ring pos skip lengths] :as state}]
+  (let [[segment-length & rest-lengths] lengths
+        
+        ;; `cycle` guarantees we'll wrap around to get the full segment length
+        reversed-segment (->> ring
+                           cycle
+                           (drop pos)
+                           (take segment-length)
+                           reverse)
+                         
+        ;; Splat the replacement in, not caring if it goes over the end of the ring's normal size
+        initial-replacement (concat (take pos ring)
+                                    reversed-segment
+                                    (drop (+ pos segment-length) ring))
+                                  
+        ;; Split off the part that's sticking off the end, if any
+        [base-replacement extra-at-end] (split-at ring-length initial-replacement)
+        
+        ;; Stick the part that was hanging off the end back at the beginning
+        pasted-replacement (->> base-replacement
+                             (drop (count extra-at-end))
+                             (concat extra-at-end))]
+
+    {:ring pasted-replacement
+     :pos (mod (+ pos segment-length skip) ring-length)
+     :skip (inc skip)
      :lengths rest-lengths}))
 
-(->> {:hash-list (range 256)
-      :current-pos 0
-      :skip-size 0
-      :lengths input}
-  (iterate step)
-  (drop-while :lengths)
-  first
-  :hash-list
+(defn knot
+  [start-state]
+  (->> start-state
+     (iterate twist)
+     (drop-while :lengths)
+     first
+     :ring))
+
+(defn make-starting-state
+  [lengths]
+  {:ring starting-ring
+   :pos 0
+   :skip 0
+   :lengths lengths})
+
+(defn str->lengths
+  [s]
+  (->> s
+    (map int)
+    (#(concat % hash-lengths-suffix))
+    cycle
+    (take (* 64 (+ (count hash-lengths-suffix) (count s))))))
+
+(defn knot-lengths
+  [lengths]
+  (knot (make-starting-state lengths)))
+
+(defn knot-string
+  [s]
+  (knot-lengths (str->lengths s)))
+
+(->> input
+  knot-lengths
   (take 2)
   (apply *))
 
-(->> {:hash-list (range 256)
-      :current-pos 0
-      :skip-size 0
-      :lengths (->> str-input
-                 (map int)
-                 (#(concat % [17 31 73 47 23]))
-                 cycle
-                 (take (* (+ 5 (count str-input)) 64)))}
-  (iterate step)
-  (drop-while :lengths)
-  first
-  :hash-list
+(->> str-input
+  knot-string
   (partition 16)
   (map #(apply bit-xor %))
   (map #(format "%02x" %))
